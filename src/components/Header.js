ccf54a7238dbd04f6244491ae1f3cf5bf4135d71
DESKTOP-9O03AF3\user2 <nfloros92@gmail.com>
2023-03-09 17:34:33 +0000

    added redirect to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,11 @@ import UserInterface from "./UserInterface";
 const Header = () => {
   return (
     <div className="header">
-      <h1 className="title">People's News</h1>
+      <h1 className="white-text">
+        <a href="/" className="header-nav">
+          News from Code It
+        </a>
+      </h1>
       <UserInterface />
     </div>
   );
